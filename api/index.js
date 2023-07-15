e0906cc4c2173c20d9329c23e669533751a06055
ricardomadeira93 <ricardomadeira1993@gmail.com>
2023-07-15 23:35:18 +0100

    Login Functionality

--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ import cors from "cors";
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import User from "./models/UserModel.js";
+import jwt from "jsonwebtoken";
 const app = express();
 
+const secret = "efwfwefewfwefhth45";
 const salt = bcrypt.genSaltSync(10);
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(express.json());
 
 mongoose.connect(
@@ -17,12 +19,29 @@ mongoose.connect(
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
   try {
-    const userDoc = await User.create({ username, password:bcrypt.hashSync(password,salt) });
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
     res.json(userDoc);
   } catch (e) {
     res.status(400).json(e);
   }
 });
 
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    // logged in
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json("ok");
+    });
+  } else {
+    res.status(400).json("wrong credentials");
+  }
+});
+
 app.listen(3000);
-//ignore
--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,21 +1,48 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
+import { useState } from "react";
+import { Link, Navigate, redirect } from "react-router-dom";
 const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  async function login(ev) {
+    ev.preventDefault();
+    const response = await fetch("http://localhost:3000/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+    if (response.ok) {
+      setRedirect(true);
+    } else {
+      alert("Wrong Credentials");
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
+
   return (
     <div>
-      <form action="">
-      <h1 className="text-center text-2xl font-bold mb-5 text-black">Login Page</h1>
+      <form onSubmit={login}>
+        <h1 className="text-center text-2xl font-bold mb-5 text-black">
+          Login Page
+        </h1>
         <input
           className="block mb-1 w-full py-1 px-1.5 border-2  bg-white border-b-1 border-black rounded-md"
           type="text"
-          name="username"
+          value={username}
+          onChange={(ev) => setUsername(ev.target.value)}
           placeholder="Username"
         />
         <input
           className="block mb-1 w-full py-1 px-1.5 border-2  bg-white border-b-1 border-black rounded-md"
           type="password"
-          name="password"
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
           placeholder="Password"
         />
         <div className="mt-2">
